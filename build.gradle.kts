51e8cc73abfd5e5bc9dd705235fa99dcf77c4692
comstering <coms1101@naver.com>
2022-11-20 23:00:26 +0900

    ➕ feat: Add spring starter web

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,7 @@ subprojects {
 
 	dependencies {
 		implementation("org.springframework.boot:spring-boot-starter-actuator")
+		implementation("org.springframework.boot:spring-boot-starter-web")
 		implementation("org.jetbrains.kotlin:kotlin-reflect")
 		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 		compileOnly("org.projectlombok:lombok")
