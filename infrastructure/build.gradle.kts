9b09832749c638af849ad4d1954dce1c8d8963b6
comstering <coms1101@naver.com>
2022-12-01 22:27:50 +0900

    ➕ perf: Add sub modules dependencies in infrastructure

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,6 +1,12 @@
 group = rootProject.group
 version = rootProject.version
 
+dependencies {
+    implementation(project(":domain"))
+    implementation(project(":service"))
+    implementation(project(":application"))
+}
+
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
