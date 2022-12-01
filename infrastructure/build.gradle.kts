7b532317a1a78bf16e7215883a9feddd13bf43a4
comstering <coms1101@naver.com>
2022-12-01 22:42:54 +0900

    ➕ feat: Add openfeign library dependency

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,10 +1,15 @@
 group = rootProject.group
 version = rootProject.version
 
+val springCloudOpenFeignVersion: String by rootProject
+
 dependencies {
     implementation(project(":domain"))
     implementation(project(":service"))
     implementation(project(":application"))
+
+    // Spring Cloud
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:$springCloudOpenFeignVersion")
 }
 
 tasks.getByName<Test>("test") {
