4ec081e7ecd0d81c9b60caa9ad17b37b4ff1c430
comstering <coms1101@naver.com>
2023-02-21 23:24:29 +0900

    ➕ build: Add spring batch dependency

--- batch/build.gradle.kts
+++ batch/build.gradle.kts
@@ -1,17 +1,12 @@
-plugins {
-    id("java")
-}
-
-group = "com.example"
-version = "1423ab7"
-
-repositories {
-    mavenCentral()
-}
+group = rootProject.group
+version = rootProject.version
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation(project(":domain"))
+    implementation(project(":infrastructure"))
+
+    // Spring Batch
+    implementation("org.springframework.boot:spring-boot-starter-batch")
 }
 
 tasks.getByName<Test>("test") {
