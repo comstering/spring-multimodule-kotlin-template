27868892a30ea1d6eee4c0049db8037324d63913
comstering <coms1101@naver.com>
2023-02-21 23:32:06 +0900

    👷 build: Build using jib by batch

--- batch/build.gradle.kts
+++ batch/build.gradle.kts
@@ -1,5 +1,12 @@
+import java.io.ByteArrayOutputStream
+
 group = rootProject.group
 version = rootProject.version
+
+val springdocOpenapiVersion: String by project
+
+val imageRegistry: String by project
+val serviceName: String by project
 
 dependencies {
     implementation(project(":domain"))
@@ -7,6 +14,22 @@ dependencies {
 
     // Spring Batch
     implementation("org.springframework.boot:spring-boot-starter-batch")
+}
+
+jib {
+    to {
+        image = "$imageRegistry/$serviceName-batch"
+        tags = setOf(getGitCurrentBranch())
+    }
+}
+
+fun getGitCurrentBranch(): String {
+    val stdout = ByteArrayOutputStream()
+    exec {
+        commandLine = listOf("git", "rev-parse", "--abbrev-ref", "HEAD")
+        standardOutput = stdout
+    }
+    return stdout.toString().trim()
 }
 
 tasks.getByName<Test>("test") {
