4032650ea874670fb1f3af37639c1a4d2bb2ebfb
comstering <coms1101@naver.com>
2023-02-27 00:28:22 +0900

    📌 merge: Update JDBC Driver dependency

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -3,16 +3,18 @@ version = rootProject.version
 
 val springCloudAWSVersion: String by rootProject
 val awsSecretsMangerJDBCVersion: String by rootProject
+val postgresqlVersion: String by rootProject
 
 dependencies {
     implementation(project(":domain"))
 
-    // JDBC Driver
-    implementation("org.postgresql:postgresql:42.5.4")
-
     // AWS Secrets Manager
     implementation("org.springframework.cloud:spring-cloud-starter-aws-secrets-manager-config:$springCloudAWSVersion")
     implementation("com.amazonaws.secretsmanager:aws-secretsmanager-jdbc:$awsSecretsMangerJDBCVersion")
+
+    // JDBC Driver
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql:$postgresqlVersion")
 }
 
 tasks.getByName<Test>("test") {
